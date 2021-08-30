0587d39a4e58fe6df6980c1dd9520b8986375541
hyun6ik <fkanwm@naver.com>
2021-08-30 23:24:29 +0900

    [VELOG-35] add : 게시판 서버 게시글 수정하기 테스트코드 작성

---
+++ potato_velog_board/src/test/java/com/example/potato_velog_board/domain/service/board/BoardServiceTest.java
@@ -0,0 +1,113 @@
+package com.example.potato_velog_board.domain.service.board;
+
+import com.example.potato_velog_board.config.StorageConfig;
+import com.example.potato_velog_board.domain.entity.Board;
+import com.example.potato_velog_board.domain.entity.ImageType;
+import com.example.potato_velog_board.domain.repository.BoardRepository;
+import com.example.potato_velog_board.exception.NotFoundException;
+import com.example.potato_velog_board.utils.error.ErrorCode;
+import com.example.potato_velog_board.web.dto.request.*;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application.yml")
+class BoardServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    BoardService boardService;
+    @Autowired
+    BoardRepository boardRepository;
+
+    @AfterEach
+    public void init() {
+        em.flush();
+        em.clear();
+    }
+
+
+    @Test
+    @DisplayName("게시판 게시글 생성 성공")
+    void createBoard() {
+
+        //given
+        final BoardRequest boardRequest = TestServiceUtils.createBoardRequest();
+
+        String uuid = "123";
+        //when
+        final Board saveBoard = boardService.createBoard(boardRequest, uuid);
+        //then
+        assertThat(saveBoard.getTitle()).isEqualTo("게시글1");
+        assertThat(saveBoard.getContent()).isEqualTo("내용");
+        assertThat(saveBoard.getBoardSeries().getName()).isEqualTo("JAVA");
+        assertThat(saveBoard.getBoardImageList().size()).isEqualTo(2);
+        assertThat(saveBoard.getBoardImageList()).extracting("originalImageName")
+                .containsExactly("test1.png", "test2.png");
+        assertThat(saveBoard.getHashTagList().size()).isEqualTo(2);
+        assertThat(saveBoard.getHashTagList()).extracting("name")
+                .containsExactly("JAVA", "Spring");
+
+    }
+
+
+    @Test
+    @DisplayName("게시판 게시글 수정 성공")
+    void updateBoard() {
+        //given
+        String uuid = "123";
+        final BoardRequest boardRequest = TestServiceUtils.createBoardRequest();
+        boardService.createBoard(boardRequest,uuid);
+
+        final BoardRequest updateRequest = TestServiceUtils.updateBoardRequest();
+
+        //when
+        final Board updateBoard = boardService.updateBoard(updateRequest, 1L, uuid);
+        //then
+        assertThat(updateBoard.getId()).isEqualTo(1L);
+        assertThat(updateBoard.getTitle()).isEqualTo("1번 게시글 수정");
+        assertThat(updateBoard.getContent()).isEqualTo("수정된 내용");
+        assertThat(updateBoard.getBoardSeries().getName()).isEqualTo("UPDATE");
+        assertThat(updateBoard.getBoardSeries().getId()).isEqualTo(1L);
+        assertThat(updateBoard.getBoardInfo().getIntroduce()).isEqualTo("자기소개 수정");
+        assertThat(updateBoard.getBoardInfo().getId()).isEqualTo(1L);
+        assertThat(updateBoard.getBoardImageList().size()).isEqualTo(1);
+        assertThat(updateBoard.getBoardImageList()).extracting("originalImageName").containsExactly("updatetest1.png");
+        assertThat(updateBoard.getHashTagList().size()).isEqualTo(3);
+        assertThat(updateBoard.getHashTagList()).extracting("name")
+                .containsExactly("UPDATE_JAVA", "UPDATE_Spring", "UPDATE_JPA");
+    }
+
+    @Test
+    @DisplayName("게시판 게시글 수정 실패 - 해당 게시글 못찾았을 때")
+    void updateBoard_fail() {
+        //given
+        String uuid = "123";
+        final BoardRequest boardRequest = TestServiceUtils.createBoardRequest();
+        boardService.createBoard(boardRequest,uuid);
+        final BoardRequest updateRequest = TestServiceUtils.updateBoardRequest();
+        //then
+        assertThatThrownBy(() -> boardService.updateBoard(updateRequest, 2L, uuid))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage(ErrorCode.NOT_FOUND_EXCEPTION_BOARD.getMessage());
+
+    }
+
+
+
+
+
+
+}
---
+++ potato_velog_board/src/test/java/com/example/potato_velog_board/domain/service/board/TestServiceUtils.java
@@ -0,0 +1,51 @@
+package com.example.potato_velog_board.domain.service.board;
+
+import com.example.potato_velog_board.domain.entity.ImageType;
+import com.example.potato_velog_board.web.dto.request.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestServiceUtils {
+
+    public static BoardRequest createBoardRequest() {
+        final BoardSeriesRequest boardSeriesRequest = new BoardSeriesRequest("JAVA");
+        final BoardInfoRequest boardInfoRequest = new BoardInfoRequest("자기소개", true, "/@hyun6ik/test");
+
+        List<BoardImageRequest> boardImageRequestList = new ArrayList<>();
+        final BoardImageRequest boardImageRequest1 = new BoardImageRequest("test1.png", "amazons3/.....",
+                "amazonserver/userUuid/uuid_test1.png", ImageType.CONTENT, "![]{}");
+        final BoardImageRequest boardImageRequest2 = new BoardImageRequest("test2.png", "amazons3/.....",
+                "amazonserver/userUuid/uuid_test2.png", ImageType.CONTENT, "![]{}");
+        boardImageRequestList.add(boardImageRequest1);
+        boardImageRequestList.add(boardImageRequest2);
+
+        List<HashTagRequest> hashTagRequestList = new ArrayList<>();
+        final HashTagRequest hashTagRequest1 = new HashTagRequest("JAVA");
+        final HashTagRequest hashTagRequest2 = new HashTagRequest("Spring");
+        hashTagRequestList.add(hashTagRequest1);
+        hashTagRequestList.add(hashTagRequest2);
+
+        return new BoardRequest("게시글1", "내용", boardSeriesRequest, boardInfoRequest, boardImageRequestList, hashTagRequestList);
+    }
+
+    public static BoardRequest updateBoardRequest(){
+        final BoardSeriesRequest boardSeriesRequest = new BoardSeriesRequest("UPDATE");
+        final BoardInfoRequest boardInfoRequest = new BoardInfoRequest("자기소개 수정", false, "/@hyun6ik/update");
+
+        List<BoardImageRequest> boardImageRequestList = new ArrayList<>();
+        final BoardImageRequest boardImageRequest = new BoardImageRequest("updatetest1.png", "amazons3/.....",
+                "amazonserver/userUuid/uuid_updatetest1.png", ImageType.CONTENT, "![]{}");
+        boardImageRequestList.add(boardImageRequest);
+
+        List<HashTagRequest> hashTagRequestList = new ArrayList<>();
+        final HashTagRequest hashTagRequest1 = new HashTagRequest("UPDATE_JAVA");
+        final HashTagRequest hashTagRequest2 = new HashTagRequest("UPDATE_Spring");
+        final HashTagRequest hashTagRequest3 = new HashTagRequest("UPDATE_JPA");
+        hashTagRequestList.add(hashTagRequest1);
+        hashTagRequestList.add(hashTagRequest2);
+        hashTagRequestList.add(hashTagRequest3);
+
+        return new BoardRequest("1번 게시글 수정", "수정된 내용", boardSeriesRequest, boardInfoRequest, boardImageRequestList, hashTagRequestList);
+    }
+}
